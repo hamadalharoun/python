ef5cfc9e483fde82def559b629c6a0e00dd33626
Hamad alharoun <hamadalharoun@Hamads-MacBook.local>
2019-09-21 19:34:05 +0300

    first commit

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+time = input("Enter a number from 1 to 12:")
+items = input("Enter a noun (plural):")
+name = input("Enter a name:")
+scream = input("Enter any sentence:")
+action = input("Enter a verb:")
+
+print("It was "+ time + " o'clock when I heard a knock at the door.")
+print("I opened the door and there was a box full of " +  items  +  " with a note saying"  " \"From Mr." + name +"\"")
+print("Just as I closed the door I heard a scream " + "\"" + scream.upper() +"\"")
+print("I froze in place and all I could do was " + action)
